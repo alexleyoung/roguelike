5e1e2c6f9714995721054c4ed92e608401ad4d37
Alex Young <alexyoung0119@gmail.com>
2025-01-31 16:53:45 -0600

    add method to print dungeon

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 #include "gen/gen.h"
 
+void print_dungeon(dungeon *dungeon) {
+    int i, j;
+    for (i = 0; i < DUNGEON_WIDTH; i++) {
+        for (j = 0; j < DUNGEON_HEIGHT; j++) {
+            printf("%c", dungeon->tiles[i][j].sprite);
+        }
+        printf("\n");
+    }
+}
+
 int main(int argc, char **argv) {
-    tile dungeon[DUNGEON_WIDTH][DUNGEON_HEIGHT];
+    // create a dungeon
+    dungeon *dungeon = malloc(sizeof (*dungeon));
     generate_dungeon(dungeon, 6);
+    print_dungeon(dungeon);
     return 0;
 }
