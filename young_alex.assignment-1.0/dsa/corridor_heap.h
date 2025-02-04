c46cc5f30f97f3768ba2ff7cc8c59f6a97a61e53
Alex Young <alexyoung0119@gmail.com>
2025-02-04 13:30:49 -0600

    add exit condition if dijkstras fails to find path due to incorrect edge weights

---
+++ young_alex.assignment-1.0/dsa/corridor_heap.h
@@ -0,0 +1,22 @@
+#ifndef HEAP_H
+#define HEAP_H
+
+#include "types.h"
+
+#define DEFAULT_HEAP_CAPACITY 100
+
+typedef struct heap {
+    point*points;
+    int *weights;
+    int size;
+    int capacity;
+} heap;
+
+int heap_init(heap *h);
+int heap_destroy(heap *h);
+int heap_push(heap *h, point p, int weight);
+int heap_pop(heap *h, point *p, int *weight);
+int heap_peek(heap *h, point *p, int *weight);
+int heap_is_empty(heap *h);
+
+#endif
