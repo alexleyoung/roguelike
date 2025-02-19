3a2c980261274594077d5e2990b86d07ce08083c
Alex Young <alexyoung0119@gmail.com>
2025-02-18 18:44:42 -0600

    fix tunneling pathfinding bug in dijkstra after asking prof. had to use source hardness, not neighbor hardness, for weight calculation

--- monsters/pathfinding.c
+++ monsters/pathfinding.c
@@ -16,11 +16,12 @@ int calc_dists(dungeon *dungeon, int map[DUNGEON_HEIGHT][DUNGEON_WIDTH], point s
     corridor_heap h;
     corridor_heap_init(&h);
     corridor_heap_push(&h, source, 0);
-    map[source.r][source.c] = 0;
+
     while (!corridor_heap_is_empty(&h)) {
         point p; // point
         int d; // distance
         corridor_heap_pop(&h, &p, &d);
+        int hardness = dungeon->tiles[p.r][p.c].hardness;
 
         for (int r = p.r - 1; r <= p.r + 1; r++) {
             for (int c = p.c - 1; c <= p.c + 1; c++) {
@@ -28,12 +29,12 @@ int calc_dists(dungeon *dungeon, int map[DUNGEON_HEIGHT][DUNGEON_WIDTH], point s
                     (r < 1 || r > DUNGEON_HEIGHT - 2 || c < 1 || c > DUNGEON_WIDTH - 2)) {
                     continue;
                 }
-                int hardness = dungeon->tiles[r][c].hardness;
                 if (!enable_tunnels && hardness) { // if tunnels disabled and hardness not 0, skip
                     continue;
                 }
 
                 int w = 1 + (hardness / 85);
+                // if the cost of current (d) > cost of the current node + 1, replace with current node + + 1 cost
                 int new_dist = d + w;
                 if (new_dist < map[r][c]) {
                     map[r][c] = new_dist;
