8c2c66bae925e45447405f30e60d41fbf17c42d2
Alex Young <alexyoung0119@gmail.com>
2025-02-23 00:09:09 -0600

    fix putting addresses of locally defined character structs, which go on the stack, in my dungeon monster maps beacuse the memory gets destroyed after the function returns

--- characters/movement.c
+++ characters/movement.c
@@ -7,8 +7,6 @@
 int move(dungeon *d, character *c) {
     if (c->traits == PLAYER_TRAIT) {
         // RANDOM PLAYER MOVEMENT
-        // empty current location in pointer map
-        d->character_map[c->pos.r][c->pos.c] = NULL;
 
         // randomly move
         d->player_pos.r += rand() % 3 - 1;
@@ -20,15 +18,25 @@ int move(dungeon *d, character *c) {
             d->player_pos.c = c->pos.c;
         }
 
-        c->pos = d->player_pos;
+        if (d->player_pos.r != c->pos.r || d->player_pos.c != c->pos.c) {
+            // empty current location in pointer map
+            d->character_map[c->pos.r][c->pos.c] = NULL;
+
+            // update dist maps
+            calc_dists(d, d->dists, d->player_pos, 0);
+            calc_dists(d, d->tunnel_dists, d->player_pos, 1);
+
+            // update character's pos
+            c->pos = d->player_pos;
+        } else {
+            return 0;
+        }
+
 
         if (d->tiles[c->pos.r][c->pos.c].sprite == ' ') {
             d->tiles[c->pos.r][c->pos.c].sprite = '#';
         }
 
-        // update dist maps
-        calc_dists(d, d->dists, d->player_pos, 0);
-        calc_dists(d, d->tunnel_dists, d->player_pos, 1);
 
         // move pointer to new pos
         d->character_map[c->pos.r][c->pos.c] = c;
