df5a944d3d1546e42aa242ae4ba666dd016db7a8
Alex Young <alexyoung0119@gmail.com>
2025-04-15 15:53:53 -0500

    finish refactor of descriptions

--- include/descriptions.hpp
+++ include/descriptions.hpp
@@ -1,8 +1,6 @@
 #ifndef DESCRIPTIONS_HPP
 #define DESCRIPTIONS_HPP
 
-#include <iostream>
-#include <sstream>
 #include <string>
 #include <vector>
 
@@ -11,6 +9,19 @@
 #include <saves.hpp>
 
 enum COLOR { RED, GREEN, BLUE, CYAN, YELLOW, MAGENTA, WHITE, BLACK };
+#define NUM_COLORS 8
+enum ABILITY {
+  SMART,
+  TELE,
+  TUNNEL,
+  ERRATIC,
+  PASS,
+  PICKUP,
+  DESTROY,
+  UNIQ,
+  BOSS
+};
+#define NUM_ABILITIES 9
 enum OBJECT_TYPE {
   WEAPON,
   OFFHAND,
@@ -34,41 +45,31 @@ enum OBJECT_TYPE {
 };
 
 class Monster_Description {
-public:
-  // terminated by \n
+private:
   std::string name;
-  // must terminate by byte 78 with \n
-  std::string description;
-  // space-separated list of colors
-  std::string color;
-  // TODO: MAKE DICE
-  std::string speed;
-  // space-separated keywords
-  std::string abilities;
-  std::string hp;
-  std::string ad;
-  std::string symbol;
-  std::string rarity;
+  std::string desc;
+  COLOR color[NUM_COLORS];
+  Dice speed;
+  ABILITY abil[NUM_ABILITIES];
+  Dice hp;
+  Dice dam;
+  char symb;
+  int rrty;
 
-  Monster_Description(std::string name, std::string description,
-                      std::string color, std::string speed,
-                      std::string abilities, std::string hp, std::string ad,
-                      std::string symbol, std::string rarity)
-      : name(name), description(description), color(color), speed(speed),
-        abilities(abilities), hp(hp), ad(ad), symbol(symbol), rarity(rarity) {};
-  ~Monster_Description() {};
+public:
+  Monster_Description() {}
+  ~Monster_Description() {}
 
-  void print_info() {
-    std::cout << name << std::endl;
-    std::cout << description;
-    std::cout << symbol << std::endl;
-    std::cout << color << std::endl;
-    std::cout << speed << std::endl;
-    std::cout << abilities << std::endl;
-    std::cout << hp << std::endl;
-    std::cout << ad << std::endl;
-    std::cout << rarity << std::endl;
-  }
+  bool set_name(const std::string &);
+  bool set_desc(const std::string &);
+  bool set_color(const std::string &);
+  bool set_speed(const std::string &);
+  bool set_hp(const std::string &);
+  bool set_dam(const std::string &);
+  bool set_abil(const std::string &);
+  bool set_symb(const std::string &);
+  bool set_rrty(const std::string &);
+  void print_info();
 };
 
 class Object_Description {
@@ -76,7 +77,7 @@ private:
   std::string name;
   std::string desc;
   OBJECT_TYPE type;
-  COLOR color[8];
+  COLOR color[NUM_COLORS];
   Dice hit;
   Dice dam;
   Dice dodge;
@@ -90,341 +91,27 @@ private:
 
 public:
   Object_Description() {}
-  ~Object_Description() {};
+  ~Object_Description() {}
 
-  bool set_name(std::string name) {
-    this->name = name;
-    return true;
-  }
+  bool set_name(const std::string &);
+  bool set_desc(const std::string &);
+  bool set_type(const std::string &);
+  bool set_color(const std::string &);
 
-  bool set_desc(std::string desc) {
-    this->desc = desc;
-    return true;
-  }
-
-  bool set_type(std::string type) {
-    if (type == "WEAPON")
-      this->type = WEAPON;
-    else if (type == "OFFHAND")
-      this->type = OFFHAND;
-    else if (type == "RANGED")
-      this->type = RANGED;
-    else if (type == "ARMOR")
-      this->type = ARMOR;
-    else if (type == "HELMET")
-      this->type = HELMET;
-    else if (type == "CLOAK")
-      this->type = CLOAK;
-    else if (type == "GLOVES")
-      this->type = GLOVES;
-    else if (type == "BOOTS")
-      this->type = BOOTS;
-    else if (type == "RING")
-      this->type = RING;
-    else if (type == "AMULET")
-      this->type = AMULET;
-    else if (type == "LIGHT")
-      this->type = LIGHT;
-    else if (type == "SCROLL")
-      this->type = SCROLL;
-    else if (type == "BOOK")
-      this->type = BOOK;
-    else if (type == "FLASK")
-      this->type = FLASK;
-    else if (type == "GOLD")
-      this->type = GOLD;
-    else if (type == "AMMUNITION")
-      this->type = AMMUNITION;
-    else if (type == "FOOD")
-      this->type = FOOD;
-    else if (type == "WAND")
-      this->type = WAND;
-    else if (type == "CONTAINER")
-      this->type = CONTAINER;
-    else {
-      return false;
-    }
-    return true;
-  }
-
-  bool set_color(std::string color) {
-    int count = 0;
-    std::stringstream ss(color);
-
-    std::string cur_color;
-    while (count < 8 && std::getline(ss, cur_color, ' ')) {
-      if (cur_color == "RED")
-        this->color[count] = RED;
-      else if (cur_color == "GREEN")
-        this->color[count] = GREEN;
-      else if (cur_color == "BLUE")
-        this->color[count] = BLUE;
-      else if (cur_color == "CYAN")
-        this->color[count] = CYAN;
-      else if (cur_color == "YELLOW")
-        this->color[count] = YELLOW;
-      else if (cur_color == "MAGENTA")
-        this->color[count] = MAGENTA;
-      else if (cur_color == "WHITE")
-        this->color[count] = WHITE;
-      else if (cur_color == "BLACK")
-        this->color[count] = BLACK;
-      else {
-        continue;
-      }
-      count++;
-    }
-
-    return true;
-  }
-
-  bool set_hit(std::string hit) {
-    Dice hit_dice;
-    std::string val;
-
-    try {
-      std::stringstream ss(hit);
-      // base
-      std::getline(ss, val, '+');
-      hit_dice.set_base(std::stoi(val));
-
-      // # of dice
-      std::getline(ss, val, 'd');
-      hit_dice.set_number(std::stoi(val));
-
-      // sides
-      std::getline(ss, val);
-      hit_dice.set_sides(std::stoi(val));
-    } catch (...) {
-      return false;
-    }
-
-    this->hit = hit_dice;
-    return true;
-  }
-
-  bool set_dam(std::string dam) {
-    Dice dam_dice;
-    std::string val;
-
-    try {
-      std::stringstream ss(dam);
-      // base
-      std::getline(ss, val, '+');
-      dam_dice.set_base(std::stoi(val));
-
-      // # of dice
-      std::getline(ss, val, 'd');
-      dam_dice.set_number(std::stoi(val));
-
-      // sides
-      std::getline(ss, val);
-      dam_dice.set_sides(std::stoi(val));
-    } catch (...) {
-      return false;
-    }
-
-    this->dam = dam_dice;
-    return true;
-  }
-
-  bool set_dodge(std::string dodge) {
-    Dice dodge_dice;
-    std::string val;
-
-    try {
-      std::stringstream ss(dodge);
-      // base
-      std::getline(ss, val, '+');
-      dodge_dice.set_base(std::stoi(val));
-
-      // # of dice
-      std::getline(ss, val, 'd');
-      dodge_dice.set_number(std::stoi(val));
-
-      // sides
-      std::getline(ss, val);
-      dodge_dice.set_sides(std::stoi(val));
-    } catch (...) {
-      return false;
-    }
-
-    this->dodge = dodge_dice;
-    return true;
-  }
-
-  bool set_def(std::string def) {
-    Dice def_dice;
-    std::string val;
-
-    try {
-      std::stringstream ss(def);
-      // base
-      std::getline(ss, val, '+');
-      def_dice.set_base(std::stoi(val));
-
-      // # of dice
-      std::getline(ss, val, 'd');
-      def_dice.set_number(std::stoi(val));
-
-      // sides
-      std::getline(ss, val);
-      def_dice.set_sides(std::stoi(val));
-    } catch (...) {
-      return false;
-    }
-
-    this->def = def_dice;
-    return true;
-  }
-
-  bool set_weight(std::string weight) {
-    Dice weight_dice;
-    std::string val;
-
-    try {
-      std::stringstream ss(weight);
-      // base
-      std::getline(ss, val, '+');
-      weight_dice.set_base(std::stoi(val));
-
-      // # of dice
-      std::getline(ss, val, 'd');
-      weight_dice.set_number(std::stoi(val));
-
-      // sides
-      std::getline(ss, val);
-      weight_dice.set_sides(std::stoi(val));
-    } catch (...) {
-      return false;
-    }
-
-    this->def = weight_dice;
-    return true;
-  }
-
-  bool set_speed(std::string speed) {
-    Dice speed_dice;
-    std::string val;
-
-    try {
-      std::stringstream ss(speed);
-      // base
-      std::getline(ss, val, '+');
-      speed_dice.set_base(std::stoi(val));
-
-      // # of dice
-      std::getline(ss, val, 'd');
-      speed_dice.set_number(std::stoi(val));
-
-      // sides
-      std::getline(ss, val);
-      speed_dice.set_sides(std::stoi(val));
-    } catch (...) {
-      return false;
-    }
-
-    this->speed = speed_dice;
-    return true;
-  }
-
-  bool set_attr(std::string attr) {
-    Dice attr_dice;
-    std::string val;
-
-    try {
-      std::stringstream ss(attr);
-
-      // base
-      std::getline(ss, val, '+');
-      attr_dice.set_base(std::stoi(val));
-
-      // # of dice
-      std::getline(ss, val, 'd');
-      attr_dice.set_number(std::stoi(val));
-
-      // sides
-      std::getline(ss, val);
-      attr_dice.set_sides(std::stoi(val));
-    } catch (...) {
-      return false;
-    }
-
-    this->attr = attr_dice;
-    return true;
-  }
-
-  bool set_val(std::string val) {
-    Dice val_dice;
-    std::string dice_val;
-
-    try {
-      std::stringstream ss(val);
-
-      // base
-      std::getline(ss, dice_val, '+');
-      val_dice.set_base(std::stoi(dice_val));
-
-      // # of dice
-      std::getline(ss, dice_val, 'd');
-      val_dice.set_number(std::stoi(dice_val));
-
-      // sides
-      std::getline(ss, dice_val);
-      val_dice.set_sides(std::stoi(dice_val));
-    } catch (...) {
-      return false;
-    }
-
-    this->val = val_dice;
-    return true;
-  }
-
-  bool set_art(std::string art) {
-    if (art == "FALSE")
-      this->art = false;
-    else if (art == "TRUE")
-      this->art = true;
-    else
-      return false;
-    return true;
-  }
-
-  bool set_rrty(std::string rrty) {
-    try {
-      this->rrty = std::stoi(rrty);
-    } catch (...) {
-      return false;
-    }
-    return true;
-  }
-
-  void print_info() {
-    std::cout << "Name: " << name << "\n";
-    std::cout << "Description:\n" << desc;
-    std::cout << "Type: " << type << "\n";
-    std::cout << "Color(s): ";
-    for (int i = 0; i < 8; ++i) {
-      // Assuming COLOR has a sentinel like COLOR_NONE or similar to mark unused
-      // slots Otherwise, check against an internal count or null/default value
-      std::cout << color[i] << " ";
-    }
-    std::cout << "\n";
-
-    std::cout << "Hit: " << hit << "\n";
-    std::cout << "Damage: " << dam << "\n";
-    std::cout << "Dodge: " << dodge << "\n";
-    std::cout << "Defense: " << def << "\n";
-    std::cout << "Weight: " << weight << "\n";
-    std::cout << "Speed: " << speed << "\n";
-    std::cout << "Attribute: " << attr << "\n";
-    std::cout << "Value: " << val << "\n";
-    std::cout << "Artifact: " << (art ? "Yes" : "No") << "\n";
-    std::cout << "Rarity: " << rrty << "\n";
-  }
+  bool set_hit(const std::string &);
+  bool set_dam(const std::string &);
+  bool set_dodge(const std::string &);
+  bool set_def(const std::string &);
+  bool set_weight(const std::string &);
+  bool set_speed(const std::string &);
+  bool set_attr(const std::string &);
+  bool set_val(const std::string &);
+  bool set_art(const std::string &);
+  bool set_rrty(const std::string &);
+  void print_info();
 };
 
-int load_monster_descriptions(const char *);
-int load_object_descriptions(const char *);
+std::vector<Monster_Description> load_monster_descriptions(const char *);
+std::vector<Object_Description> load_object_descriptions(const char *);
 
 #endif
--- src/descriptions.cpp
+++ src/descriptions.cpp
@@ -2,103 +2,515 @@
 #include <iostream>
 #include <ostream>
 #include <sstream>
+#include <vector>
 
 #include "descriptions.hpp"
 #include "saves.hpp"
 
-void parse_monster(std::ifstream &file, std::string &line) {
+/*
+ * UTIL METHODS
+ */
+bool parse_dice(const std::string &str, Dice &target) {
+  try {
+    std::stringstream ss(str);
+    std::string val;
+
+    std::getline(ss, val, '+');
+    target.set_base(std::stoi(val));
+
+    std::getline(ss, val, 'd');
+    target.set_number(std::stoi(val));
+
+    std::getline(ss, val);
+    target.set_sides(std::stoi(val));
+  } catch (...) {
+    return false;
+  }
+  return true;
+}
+
+const char *to_string(COLOR c) {
+  switch (c) {
+  case RED:
+    return "RED";
+  case GREEN:
+    return "GREEN";
+  case BLUE:
+    return "BLUE";
+  case CYAN:
+    return "CYAN";
+  case YELLOW:
+    return "YELLOW";
+  case MAGENTA:
+    return "MAGENTA";
+  case WHITE:
+    return "WHITE";
+  case BLACK:
+    return "BLACK";
+  default:
+    return "UNKNOWN_COLOR";
+  }
+}
+
+const char *to_string(ABILITY a) {
+  switch (a) {
+  case SMART:
+    return "SMART";
+  case TELE:
+    return "TELE";
+  case TUNNEL:
+    return "TUNNEL";
+  case ERRATIC:
+    return "ERRATIC";
+  case PASS:
+    return "PASS";
+  case PICKUP:
+    return "PICKUP";
+  case DESTROY:
+    return "DESTROY";
+  case UNIQ:
+    return "UNIQ";
+  case BOSS:
+    return "BOSS";
+  default:
+    return "";
+  }
+}
+
+const char *to_string(OBJECT_TYPE t) {
+  switch (t) {
+  case WEAPON:
+    return "WEAPON";
+  case OFFHAND:
+    return "OFFHAND";
+  case RANGED:
+    return "RANGED";
+  case ARMOR:
+    return "ARMOR";
+  case HELMET:
+    return "HELMET";
+  case CLOAK:
+    return "CLOAK";
+  case GLOVES:
+    return "GLOVES";
+  case BOOTS:
+    return "BOOTS";
+  case RING:
+    return "RING";
+  case AMULET:
+    return "AMULET";
+  case LIGHT:
+    return "LIGHT";
+  case SCROLL:
+    return "SCROLL";
+  case BOOK:
+    return "BOOK";
+  case FLASK:
+    return "FLASK";
+  case GOLD:
+    return "GOLD";
+  case AMMUNITION:
+    return "AMMUNITION";
+  case FOOD:
+    return "FOOD";
+  case WAND:
+    return "WAND";
+  case CONTAINER:
+    return "CONTAINER";
+  default:
+    return "";
+  }
+}
+
+/*
+ * MONSTER_DESCRIPTION CLASS IMPLEMENTATIONS
+ */
+bool Monster_Description::set_name(const std::string &name) {
+  this->name = name;
+  return true;
+}
+
+bool Monster_Description::set_desc(const std::string &desc) {
+  this->desc = desc;
+  return true;
+}
+
+bool Monster_Description::set_color(const std::string &color_str) {
+  int count = 0;
+  std::stringstream ss(color_str);
+  std::string token;
+  while (count < NUM_COLORS && std::getline(ss, token, ' ')) {
+    if (token == "RED")
+      color[count++] = RED;
+    else if (token == "GREEN")
+      color[count++] = GREEN;
+    else if (token == "BLUE")
+      color[count++] = BLUE;
+    else if (token == "CYAN")
+      color[count++] = CYAN;
+    else if (token == "YELLOW")
+      color[count++] = YELLOW;
+    else if (token == "MAGENTA")
+      color[count++] = MAGENTA;
+    else if (token == "WHITE")
+      color[count++] = WHITE;
+    else if (token == "BLACK")
+      color[count++] = BLACK;
+  }
+  return count > 0;
+}
+
+bool Monster_Description::set_speed(const std::string &s) {
+  return parse_dice(s, speed);
+}
+bool Monster_Description::set_hp(const std::string &s) {
+  return parse_dice(s, hp);
+}
+bool Monster_Description::set_dam(const std::string &s) {
+  return parse_dice(s, dam);
+}
+
+bool Monster_Description::set_abil(const std::string &s) {
+  int count = 0;
+  std::stringstream ss(s);
+  std::string token;
+  while (count < NUM_ABILITIES && std::getline(ss, token, ' ')) {
+    if (token == "SMART")
+      abil[count++] = SMART;
+    else if (token == "TELE")
+      abil[count++] = TELE;
+    else if (token == "TUNNEL")
+      abil[count++] = TUNNEL;
+    else if (token == "ERRATIC")
+      abil[count++] = ERRATIC;
+    else if (token == "PASS")
+      abil[count++] = PASS;
+    else if (token == "PICKUP")
+      abil[count++] = PICKUP;
+    else if (token == "DESTROY")
+      abil[count++] = DESTROY;
+    else if (token == "UNIQ")
+      abil[count++] = UNIQ;
+    else if (token == "BOSS")
+      abil[count++] = BOSS;
+  }
+  return count > 0;
+}
+
+bool Monster_Description::set_symb(const std::string &s) {
+  if (s.length() != 1)
+    return false;
+  symb = s[0];
+  return true;
+}
+
+bool Monster_Description::set_rrty(const std::string &s) {
+  try {
+    rrty = std::stoi(s);
+  } catch (...) {
+    return false;
+  }
+  return true;
+}
+
+void Monster_Description::print_info() {
+  std::cout << "Name: " << name << "\n";
+  std::cout << "Description:\n" << desc;
+  std::cout << "Symbol: " << symb << "\n";
+
+  std::cout << "Color(s): ";
+  for (int i = 0; i < NUM_COLORS; ++i) {
+    std::cout << color[i] << " ";
+  }
+  std::cout << "\n";
+
+  std::cout << "Speed: " << speed << "\n";
+
+  std::cout << "Abilities: ";
+  for (int i = 0; i < NUM_ABILITIES; ++i) {
+    std::cout << abil[i] << " ";
+  }
+  std::cout << "\n";
+
+  std::cout << "HP: " << hp << "\n";
+  std::cout << "Damage: " << dam << "\n";
+  std::cout << "Rarity: " << rrty << "\n";
+}
+
+/*
+ * OBJECT_DESCRIPTION CLASS IMPLEMENTATIONS
+ */
+bool Object_Description::set_name(const std::string &name) {
+  this->name = name;
+  return true;
+}
+
+bool Object_Description::set_desc(const std::string &desc) {
+  this->desc = desc;
+  return true;
+}
+
+bool Object_Description::set_type(const std::string &type_str) {
+  if (type_str == "WEAPON")
+    type = WEAPON;
+  else if (type_str == "OFFHAND")
+    type = OFFHAND;
+  else if (type_str == "RANGED")
+    type = RANGED;
+  else if (type_str == "ARMOR")
+    type = ARMOR;
+  else if (type_str == "HELMET")
+    type = HELMET;
+  else if (type_str == "CLOAK")
+    type = CLOAK;
+  else if (type_str == "GLOVES")
+    type = GLOVES;
+  else if (type_str == "BOOTS")
+    type = BOOTS;
+  else if (type_str == "RING")
+    type = RING;
+  else if (type_str == "AMULET")
+    type = AMULET;
+  else if (type_str == "LIGHT")
+    type = LIGHT;
+  else if (type_str == "SCROLL")
+    type = SCROLL;
+  else if (type_str == "BOOK")
+    type = BOOK;
+  else if (type_str == "FLASK")
+    type = FLASK;
+  else if (type_str == "GOLD")
+    type = GOLD;
+  else if (type_str == "AMMUNITION")
+    type = AMMUNITION;
+  else if (type_str == "FOOD")
+    type = FOOD;
+  else if (type_str == "WAND")
+    type = WAND;
+  else if (type_str == "CONTAINER")
+    type = CONTAINER;
+  else
+    return false;
+  return true;
+}
+
+bool Object_Description::set_color(const std::string &color_str) {
+  int count = 0;
+  std::stringstream ss(color_str);
+  std::string token;
+  while (count < NUM_COLORS && std::getline(ss, token, ' ')) {
+    if (token == "RED")
+      color[count++] = RED;
+    else if (token == "GREEN")
+      color[count++] = GREEN;
+    else if (token == "BLUE")
+      color[count++] = BLUE;
+    else if (token == "CYAN")
+      color[count++] = CYAN;
+    else if (token == "YELLOW")
+      color[count++] = YELLOW;
+    else if (token == "MAGENTA")
+      color[count++] = MAGENTA;
+    else if (token == "WHITE")
+      color[count++] = WHITE;
+    else if (token == "BLACK")
+      color[count++] = BLACK;
+  }
+  return count > 0;
+}
+
+bool Object_Description::set_hit(const std::string &s) {
+  return parse_dice(s, hit);
+}
+bool Object_Description::set_dam(const std::string &s) {
+  return parse_dice(s, dam);
+}
+bool Object_Description::set_dodge(const std::string &s) {
+  return parse_dice(s, dodge);
+}
+bool Object_Description::set_def(const std::string &s) {
+  return parse_dice(s, def);
+}
+bool Object_Description::set_weight(const std::string &s) {
+  return parse_dice(s, weight);
+}
+bool Object_Description::set_speed(const std::string &s) {
+  return parse_dice(s, speed);
+}
+bool Object_Description::set_attr(const std::string &s) {
+  return parse_dice(s, attr);
+}
+bool Object_Description::set_val(const std::string &s) {
+  return parse_dice(s, val);
+}
+
+bool Object_Description::set_art(const std::string &art_str) {
+  if (art_str == "TRUE")
+    art = true;
+  else if (art_str == "FALSE")
+    art = false;
+  else
+    return false;
+  return true;
+}
+
+bool Object_Description::set_rrty(const std::string &rrty_str) {
+  try {
+    rrty = std::stoi(rrty_str);
+  } catch (...) {
+    return false;
+  }
+  return true;
+}
+
+void Object_Description::print_info() {
+  std::cout << "Name: " << name << "\n";
+  std::cout << "Description:\n" << desc;
+  std::cout << "Type: " << type << "\n";
+
+  std::cout << "Color(s): ";
+  for (int i = 0; i < NUM_COLORS; ++i) {
+    std::cout << color[i] << " ";
+  }
+  std::cout << "\n";
+
+  std::cout << "Hit: " << hit << "\n";
+  std::cout << "Damage: " << dam << "\n";
+  std::cout << "Dodge: " << dodge << "\n";
+  std::cout << "Defense: " << def << "\n";
+  std::cout << "Weight: " << weight << "\n";
+  std::cout << "Speed: " << speed << "\n";
+  std::cout << "Attribute: " << attr << "\n";
+  std::cout << "Value: " << val << "\n";
+  std::cout << "Artifact: " << (art ? "Yes" : "No") << "\n";
+  std::cout << "Rarity: " << rrty << "\n";
+}
+
+/*
+ * FUNCTIONS
+ */
+void parse_monster(std::ifstream &file, std::string &line,
+                   std::vector<Monster_Description> &vec) {
   while (std::getline(file, line) && line != "BEGIN MONSTER") {
   }
 
-  // fields
-  std::string name;
-  std::string description;
-  std::string color;
-  std::string speed;
-  std::string abilities;
-  std::string hp;
-  std::string ad;
-  std::string symbol;
-  std::string rarity;
+  Monster_Description md;
 
-  bool create = false;
+  bool name_set = false;
+  bool desc_set = false;
+  bool color_set = false;
+  bool speed_set = false;
+  bool abil_set = false;
+  bool hp_set = false;
+  bool dam_set = false;
+  bool symb_set = false;
+  bool rrty_set = false;
+
   std::string keyword;
+  std::string data;
+
   while (std::getline(file, line)) {
     std::stringstream ss(line);
     std::getline(ss, keyword, ' ');
 
     if (keyword == "NAME") {
-      if (name.length())
+      if (name_set)
         return;
-      std::getline(ss, name);
+      std::getline(ss, data);
+      if (!md.set_name(data))
+        return;
+      name_set = true;
 
     } else if (keyword == "DESC") {
-      if (description.length())
+      if (desc_set)
         return;
+      std::string desc;
       while (std::getline(file, line) && line != ".") {
-        description += line + "\n";
+        desc += line + "\n";
       }
+      if (!md.set_desc(desc))
+        return;
+      desc_set = true;
 
     } else if (keyword == "COLOR") {
-      if (color.length())
+      if (color_set)
         return;
-      std::getline(ss, color);
+      std::getline(ss, data);
+      if (!md.set_color(data))
+        return;
+      color_set = true;
 
     } else if (keyword == "SPEED") {
-      if (speed.length())
+      if (speed_set)
         return;
-      std::getline(ss, speed);
+      std::getline(ss, data);
+      if (!md.set_speed(data))
+        return;
+      speed_set = true;
 
     } else if (keyword == "ABIL") {
-      if (abilities.length())
+      if (abil_set)
         return;
-      std::getline(ss, abilities);
+      std::getline(ss, data);
+      if (!md.set_abil(data))
+        return;
+      abil_set = true;
 
     } else if (keyword == "HP") {
-      if (hp.length())
+      if (hp_set)
         return;
-      std::getline(ss, hp);
+      std::getline(ss, data);
+      if (!md.set_hp(data))
+        return;
+      hp_set = true;
 
     } else if (keyword == "DAM") {
-      if (ad.length())
+      if (dam_set)
         return;
-      std::getline(ss, ad);
+      std::getline(ss, data);
+      if (!md.set_dam(data))
+        return;
+      dam_set = true;
 
     } else if (keyword == "SYMB") {
-      if (symbol.length())
+      if (symb_set)
         return;
-      std::getline(ss, symbol);
+      std::getline(ss, data);
+      if (!md.set_symb(data))
+        return;
+      symb_set = true;
 
     } else if (keyword == "RRTY") {
-      if (rarity.length())
+      if (rrty_set)
         return;
-      std::getline(ss, rarity);
+      std::getline(ss, data);
+      if (!md.set_rrty(data))
+        return;
+      rrty_set = true;
 
     } else if (keyword == "END") {
-      // if missing field, exit
-      if (!name.length() || !description.length() || !color.length() ||
-          !speed.length() || !abilities.length() || !hp.length() ||
-          !ad.length() || !symbol.length() || !rarity.length()) {
-        std::cout << "missing fields: " << keyword << std::endl;
-        std::cout << "Skipping monster. " << keyword << std::endl;
+      if (!(name_set && desc_set && color_set && speed_set && abil_set &&
+            hp_set && dam_set && symb_set && rrty_set)) {
+        std::cout << "Missing fields before END.\n";
+        std::cout << "Skipping monster.\n";
         return;
       }
 
-      std::cout << std::endl;
-      Monster_Description md(name, description, color, speed, abilities, hp, ad,
-                             symbol, rarity);
       md.print_info();
+      std::cout << std::endl;
+      vec.push_back(md);
       break;
 
     } else {
-      std::cout << "invalid keyword: " << keyword << std::endl;
-      std::cout << "Skipping monster. " << keyword << std::endl;
+      std::cout << "Invalid keyword: " << keyword << std::endl;
+      std::cout << "Skipping monster.\n";
       return;
     }
   }
 }
 
-void parse_object(std::ifstream &file, std::string &line) {
+void parse_object(std::ifstream &file, std::string &line,
+                  std::vector<Object_Description> &vec) {
   while (std::getline(file, line) && line != "BEGIN OBJECT") {
   }
 
@@ -253,6 +665,7 @@ void parse_object(std::ifstream &file, std::string &line) {
       // Successfully parsed the object
       od.print_info();
       std::cout << std::endl;
+      vec.push_back(od);
       break;
 
     } else {
@@ -263,13 +676,15 @@ void parse_object(std::ifstream &file, std::string &line) {
   }
 }
 
-int load_monster_descriptions(const char *fileName) {
+std::vector<Monster_Description>
+load_monster_descriptions(const char *fileName) {
   std::string path = get_save_path(fileName);
+  std::vector<Monster_Description> vec;
 
   std::ifstream file(path);
   if (!file.is_open()) {
     std::cout << "Couldn't open file: " << path << std::endl;
-    return 1;
+    return vec;
   }
 
   std::string line;
@@ -278,24 +693,25 @@ int load_monster_descriptions(const char *fileName) {
   std::getline(file, line);
   if (line != "RLG327 MONSTER DESCRIPTION 1") {
     std::cout << "Not a proper Monster Description File!" << std::endl;
-    return 1;
+    return vec;
   }
 
   // until no more lines / eof
   while (std::getline(file, line)) {
-    parse_monster(file, line);
+    parse_monster(file, line, vec);
   }
 
-  return 0;
+  return vec;
 }
 
-int load_object_descriptions(const char *fileName) {
+std::vector<Object_Description> load_object_descriptions(const char *fileName) {
   std::string path = get_save_path(fileName);
+  std::vector<Object_Description> vec;
 
   std::ifstream file(path);
   if (!file.is_open()) {
     std::cout << "Couldn't open file: " << path << std::endl;
-    return 1;
+    return vec;
   }
 
   std::string line;
@@ -304,13 +720,13 @@ int load_object_descriptions(const char *fileName) {
   std::getline(file, line);
   if (line != "RLG327 OBJECT DESCRIPTION 1") {
     std::cout << "Not a proper Monster Description File!" << std::endl;
-    return 1;
+    return vec;
   }
 
   // until no more lines / eof
   while (std::getline(file, line)) {
-    parse_object(file, line);
+    parse_object(file, line, vec);
   }
 
-  return 0;
+  return vec;
 }
